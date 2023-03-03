68fb7c54749e65cf99c556d945cccf7f2a775474
Verlect <59546942+Verlect@users.noreply.github.com>
2023-03-02 22:42:28 -0700

    Update DataUtilitiesTest.java

--- src/org/jfree/data/DataUtilitiesTest.java
+++ src/org/jfree/data/DataUtilitiesTest.java
@@ -19,18 +19,157 @@ import org.junit.*;
 public class DataUtilitiesTest {
 	Mockery mock;
 	Values2D empty;
+	KeyedValues values;
+	
+	
    
 	@BeforeClass
 	public static void setUpBeforeClass() throws Exception {
 		
 	}
+	double [][] arrNormal;
+	double [][] arrOneEmpty;
+	double [][] arrTwoEmpty;
+	double [][] arrResult;
+   
 	
 	@Before
-	
-	public void SetUp( ) {
-	
+	public void SetUp( )throws Exception  {
+		
+		
+		arrNormal = new double [][]{
+				{1,2,3},
+				{4,5,6}
+		};
+		arrOneEmpty = new double [][]{
+			{1,2},
+			{}
+		};
+		arrTwoEmpty = new double [][]{
+			{},
+			{}
+		};
 	
 		
+	}
+	
+
+	@Test
+	public void cloneTestNormal() {
+		
+		arrResult = DataUtilities.clone(arrNormal);
+		assertTrue("Should Equal", DataUtilities.equal(arrResult, arrNormal));
+	}
+	
+	
+	@Test
+	public void cloneTestOneEmpty() {
+		
+		arrResult = DataUtilities.clone(arrOneEmpty);
+		assertTrue("Should Equal", DataUtilities.equal(arrResult, arrOneEmpty));
+	}
+	
+	
+	@Test
+	public void cloneTestTwoEmpty() {
+		
+		arrResult = DataUtilities.clone(arrTwoEmpty);
+		assertTrue("Should Equal", DataUtilities.equal(arrResult, arrTwoEmpty));
+	}
+		
+	
+	
+	
+	
+	
+	
+	@Test
+	public void createNumberArrayTestInt() {
+		Number[] expected = {7,2,4};
+		double[] doubleArray = {7,2,4};
+		
+		Number[] result = DataUtilities.createNumberArray(doubleArray);
+		assertArrayEquals("Should Equal",expected,result);
+	}
+	
+	@Test
+	public void createNumberArrayTestDouble() {
+		Number[] expected = { 1.7, 2.2, 3.4 };
+		double[] doubleArray = { 1.7, 2.2, 3.4 };
+		
+		Number[] result = DataUtilities.createNumberArray(doubleArray);
+		assertArrayEquals("Should Equal",expected,result);
+	}
+	@Test
+	public void createNumberArrayTestIntNegative() {
+		Number[] expected = {-7,-2,-4};
+		double[] doubleArray = {-7,-2,-4};
+		
+		Number[] result = DataUtilities.createNumberArray(doubleArray);
+		assertArrayEquals("Should Equal",expected,result);
+	}
+	@Test
+	public void createNumberArrayTestDoubleNegative() {
+		Number[] expected = { -1.7, -2.2, -3.4 };
+		double[] doubleArray = { -1.7,- 2.2, -3.4 };
+		
+		Number[] result = DataUtilities.createNumberArray(doubleArray);
+		assertArrayEquals("Should Equal",expected,result);
+	}
+	@Test
+	public void createNumberArrayTestOneElement() {
+		Number[] expected = {6 };
+		double[] doubleArray = { 6};
+		
+		Number[] result = DataUtilities.createNumberArray(doubleArray);
+		assertArrayEquals("Should Equal",expected,result);
+	}
+	@Test
+	public void createNumberArrayTestEmpty() {
+		Number[] expected = { };
+		double[] doubleArray = { };
+		
+		Number[] result = DataUtilities.createNumberArray(doubleArray);
+		assertArrayEquals("Should Equal",expected,result);
+	}
+
+	
+	
+	
+	
+	
+	
+	@Test
+	public void createNumberArray2DNormal() {
+		Number[][] expected = { { 3.1, 1.4, 1.5}, { 1.3 , 4.1, 5.1} };
+		double[][] doubleArray = { { 3.1, 1.4, 1.5}, { 1.3 , 4.1, 5.1} };
+		Number[][] result = DataUtilities.createNumberArray2D(doubleArray);
+
+		assertArrayEquals("Should Equal", expected, result);
+	}
+	@Test
+	public void createNumberArray2DNoArray() {
+		Number[][] expected = { };
+		double[][] doubleArray = { };
+		Number[][] result = DataUtilities.createNumberArray2D(doubleArray);
+
+		assertArrayEquals("Should Equal", expected, result);
+	}
+	@Test
+	public void createNumberArray2DOneEmpty() {
+		Number[][] expected = { { 3.1, 1.4, 1.5}, {} };
+		double[][] doubleArray = { { 3.1, 1.4, 1.5}, {} };
+		Number[][] result = DataUtilities.createNumberArray2D(doubleArray);
+
+		assertArrayEquals("Should Equal", expected, result);
+	}
+	@Test
+	public void createNumberArray2DTwoEmpty() {
+		Number[][] expected = { { }, {} };
+		double[][] doubleArray = { { }, {} };
+		Number[][] result = DataUtilities.createNumberArray2D(doubleArray);
+
+		assertArrayEquals("Should Equal", expected, result);
 	}
 	
 	//Calculate Column Total
@@ -409,5 +548,100 @@ public class DataUtilitiesTest {
 		});
 		
 		assertEquals("The total has been added correctly.", 50.0, DataUtilities.calculateRowTotal(empty, 1),0.0000001d);
+	}
+	
+	@Test
+	public void twoEqualArrays() {
+		double[] [] firstarray = {{1.0,2.0,3.0,4.0,5.0}, {6.0,7.0,8.0,9.0,10.0}};
+		
+		double[] [] secondarray = {{1.0,2.0,3.0,4.0,5.0}, {6.0,7.0,8.0,9.0,10.0}};
+		
+		boolean equals = DataUtilities.equal(firstarray, secondarray);
+		
+		assertEquals("These are the same array", true, equals);
+	}
+	
+	@Test
+	public void twoNonEqualArrays() {
+		double[] [] firstarray = {{1.0,2.0,3.0,4.0,5.0}, {6.0,7.0,8.0,9.0,10.0}};
+		
+		double[] [] secondarray = {{2.0,2.0,3.0,4.0,5.0}, {6.0,7.0,8.0,9.0,10.0}};
+		
+		boolean equals = DataUtilities.equal(firstarray, secondarray);
+		
+		assertEquals("These are not the same array", false, equals);
+	}
+	
+	@Test
+	public void twoNonEqualArraysDifLength() {
+		double[] [] firstarray = {{1.0,2.0,3.0,4.0,5.0}, {6.0,7.0,8.0,9.0,10.0}};
+		
+		double[] [] secondarray = {{2.0,3.0,4.0,5.0}, {6.0,7.0,8.0,9.0,10.0}};
+		
+		boolean equals = DataUtilities.equal(firstarray, secondarray);
+		
+		assertEquals("These are not the same array", false, equals);
+	}
+	
+	@Test
+	public void nullarrayOne() {
+		double[] [] firstarray = null;
+		
+		double[] [] secondarray = {{2.0,3.0,4.0,5.0}, {6.0,7.0,8.0,9.0,10.0}};
+		
+		boolean equals = DataUtilities.equal(firstarray, secondarray);
+		
+		assertEquals("These are not the same array", false, equals);
+	}
+	
+	@Test
+	public void nullArrayTwo() {
+		double[] [] firstarray = {{1.0,2.0,3.0,4.0,5.0}, {6.0,7.0,8.0,9.0,10.0}};
+		
+		double[] [] secondarray = null;
+		
+		boolean equals = DataUtilities.equal(firstarray, secondarray);
+		
+		assertEquals("These are not the same array", false, equals);
+	}
+	
+	@Test
+	public void bothNullArray() {
+		double[] [] firstarray = null;
+		
+		double[] [] secondarray = null;
+		
+		boolean equals = DataUtilities.equal(firstarray, secondarray);
+		
+		assertEquals("These are not the same array", true, equals);
+	}
+	
+	@Test
+	public void getCumPercentageTestOne() {
+		mock = new Mockery();
+		values = mock.mock(KeyedValues.class);
+		
+		mock.checking(new Expectations() {
+			{
+				atLeast(1).of(values).getValue(0);
+				will(returnValue(10));
+				atLeast(1).of(values).getKey(0);
+				will(returnValue(10));
+				atLeast(1).of(values).getValue(1);
+				will(returnValue(20));
+				atLeast(1).of(values).getKey(1);
+				will(returnValue(11));
+				atLeast(1).of(values).getItemCount();
+				will(returnValue(2));
+			}
+		});
+		
+		assertEquals("Cumulative Percentage is not correct", 0.33333333333333, DataUtilities.getCumulativePercentages(values).getValue(0).doubleValue(), 0.0000001d );
+		
+	}
+	
+	@Test
+	public void getCumPercentageNull() {
+		
 	}
 }
